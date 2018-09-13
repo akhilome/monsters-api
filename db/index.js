ecda34f7fe2f7941615195128bd5e09eeccc9859
Kizito Akhilome <kizito@akhilo.me>
2018-09-13 13:58:27 +0100

    Make a route to handle getting all monsters in db
    - configure express as a node executable and make a GET /monsters endpoint

--- db/index.js
+++ db/index.js
@@ -3,8 +3,4 @@ const { user, host, database, password, port } = require('../secrets/dbConfig');
 
 const pool = new Pool({ user, host, database, password, port });
 
-pool.query('SELECT * FROM monsters', (err, res) => {
-  if (err) return console.error(err);
-
-  console.log(res);
-});
+module.exports = pool;
