c2eab2ce71942a557462eb7973167b6eb4df91f2
Kizito Akhilome <kizito@akhilo.me>
2018-09-13 18:45:40 +0100

    Implement error handling middleware

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const pool = require('./db');
 
 const app = express();
 
-app.get('/monsters', (request, response) => {
+app.get('/monsters', (request, response, next) => {
   pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-    if (err) return console.error(err);
+    if (err) return next(err);
     response.json(res.rows);
   });
+});
+
+app.use((err, req, res, next) => {
+  res.json(err); 
 });
 
 module.exports = app;
