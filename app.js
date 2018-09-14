16eb130b5ecbf5764678862cb387af07b09540a3
Kizito Akhilome <kizito@akhilo.me>
2018-09-14 11:36:06 +0100

    Create habitats namespace and implement necessary endpoints
    implement
    - GET /habitats
    - GET /habitats/:id
    - POST /habitats
    - PUT /habitats/:id
    - DELETE /habitats/:id

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const monstersRouter = require('./routes/monsters');
+const habitatsRouter = require('./routes/habitats');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/monsters', monstersRouter);
+app.use('/habitats', habitatsRouter);
 
 app.use((err, req, res, next) => {
   res.json(err); 
