6736b5be8fbb4e7a2baee7324b3f06ff73ac9e7f
Kizito Akhilome <kizito@akhilo.me>
2018-09-14 12:50:12 +0100

    Create lives namespace and implement necessary endpoints
    implement
    - GET /lives
    - GET /lives/conditions

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const monstersRouter = require('./routes/monsters');
 const habitatsRouter = require('./routes/habitats');
+const livesRouter = require('./routes/lives');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/monsters', monstersRouter);
 app.use('/habitats', habitatsRouter);
+app.use('/lives', livesRouter);
 
 app.use((err, req, res, next) => {
   res.json(err); 
